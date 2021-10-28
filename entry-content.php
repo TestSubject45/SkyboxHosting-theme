75aafb3b449c8c5c887a49462bcfea3de2e3841e
TestSubject45 <blakegambel@yahoo.com>
2021-10-28 15:59:57 -0600

    Initial Commit

---
+++ entry-content.php
@@ -0,0 +1,5 @@
+<div class="entry-content" itemprop="mainEntityOfPage">
+<meta itemprop="description" content="<?php echo wp_strip_all_tags( get_the_excerpt(), true ); ?>" />
+<?php the_content(); ?>
+<div class="entry-links"><?php wp_link_pages(); ?></div>
+</div>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Concert+One&family=Zen+Kaku+Gothic+Antique:wght@300;500;900&display=swap" rel="stylesheet">
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="wrapper" class="hfeed">
+<header id="header" role="banner">
+<div id="branding">
+<div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+<?php
+if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
+echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
+if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
+?>
+</div>
+<div id="site-description"<?php if ( !is_single() ) { echo ' itemprop="description"'; } ?>><?php bloginfo( 'description' ); ?></div>
+</div>
+<nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
+<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>' ) ); ?>
+</nav>
+</header>
+<div class="banner-image">
+<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'full', array( 'itemprop' => 'image' ) ); } ?>
+</div>
+<div id="container">
